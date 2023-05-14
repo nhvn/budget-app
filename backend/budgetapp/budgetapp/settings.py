abcde1ec3d365765fdf1311ef832a87b1c792968
nhvn <alannhan@ymail.com>
2023-05-14 16:38:14 -0700

    set up PostgreSQL database

--- backend/budgetapp/budgetapp/settings.py
+++ backend/budgetapp/budgetapp/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'budgetapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'budget_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
